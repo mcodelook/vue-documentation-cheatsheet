d3d7c2722a3dfa3d0957890eca85f17472ef943a
mcodelook <152505546+mcodelook@users.noreply.github.com>
2024-12-17 19:30:31 +0100

    Updates

---
+++ assets/AsyncComponentsChild-vAue0XzT.js
@@ -0,0 +1,1 @@
+import{_ as e,o as n,i as o}from"./index-C6y1L5lN.js";const c={};function t(s,r){return n(),o("p",null,"The component is mounted!")}const a=e(c,[["render",t]]);export{a as default};
---
+++ assets/useCounter-hp46ZjRl.js
@@ -0,0 +1,1 @@
+import{e as t}from"./index-C6y1L5lN.js";const s=(n=0)=>{const e=t(n);return{count:e,increment:()=>e.value++,decrement:()=>e.value--}};export{s as u};
